6da4cda1f026755407504d1469491b1640a7bc32
bdizen <bdizenweb@gmail.com>
2025-02-22 20:52:08 +0200

    Change test collector structure to eliminate reuse of code

--- tests/test_cpu.py
+++ tests/test_cpu.py
@@ -1,21 +1,8 @@
 from unittest import TestCase
-from dataclasses import asdict
-from pprint import pprint
-import time
 from pcmonitor.core.cpu import CpuMetricCollector
+from tests.test_collectors import test_collector
 
 
 class TestCpuMetricCollector(TestCase):
     def test_collector(self):
-        collector = CpuMetricCollector()
-        collector.start()
-
-        try:
-            while True:
-                metrics = collector.get_metrics()
-                if metrics:
-                    pprint(asdict(metrics))
-                time.sleep(1)
-        except KeyboardInterrupt:
-            collector.stop()
-            print("\nStopped metric collection")
+        test_collector(CpuMetricCollector())
--- tests/test_memory.py
+++ tests/test_memory.py
@@ -1,21 +1,8 @@
 from unittest import TestCase
-from dataclasses import asdict
-from pprint import pprint
-import time
 from pcmonitor.core.memory import MemoryMetricCollector
+from tests.test_collectors import test_collector
 
 
 class TestMemoryMetricCollector(TestCase):
     def test_collector(self):
-        collector = MemoryMetricCollector()
-        collector.start()
-
-        try:
-            while True:
-                metrics = collector.get_metrics()
-                if metrics:
-                    pprint(asdict(metrics))
-                time.sleep(1)
-        except KeyboardInterrupt:
-            collector.stop()
-            print("\nStopped metric collection")
+        test_collector(MemoryMetricCollector())
